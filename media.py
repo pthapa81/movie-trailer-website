7b847e26d598efbaf7fea368c6f9b8a02f5c36a9
prashant <prashant.thapa@bison.howard.edu>
2017-07-15 13:53:13 -0700

    Fixing the review comments for media.py

--- media.py
+++ media.py
@@ -8,7 +8,13 @@ class Movie():
     RANKING_SCORE = [1, 2, 3, 4, 5]
     CATEGORY = ["comedy", "adventure", "romance", "thriller"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_url, synopsis, release_year):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_url, synopsis, release_year):
+        """
+        A movie object consists of a title, story line, url to the poster img,
+        url to the youtube trailer, synopsis and a release year. It could be
+        expanded if needed based on future requirements
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -17,4 +23,5 @@ class Movie():
         self.release_year = release_year
 
     def show_trailer(self):
+        # to opens the youtube trailer video using the url instance of the obj
         webbrowser.open(self.trailer_youtube_url)
